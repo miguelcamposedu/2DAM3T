4cccaebbd35073e6299723e97e98fcc1bf728495
Miguel Campos <camposmiguel@gmail.com>
2021-04-05 13:08:30 +0200

    Skeleton app

--- skeleton-app/src/environments/environment.prod.ts
+++ skeleton-app/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDao5DpaO3H96GRD7oC2vRH90YJY_XVpks",
+    authDomain: "entreapp-7a87c.firebaseapp.com",
+    projectId: "entreapp-7a87c",
+    storageBucket: "entreapp-7a87c.appspot.com",
+    messagingSenderId: "663673157117",
+    appId: "1:663673157117:web:b0f960c9b3e344355a9594",
+    measurementId: "G-RYKXSZ6QPW"
+  }
 };
--- skeleton-app/src/environments/environment.ts
+++ skeleton-app/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDao5DpaO3H96GRD7oC2vRH90YJY_XVpks",
+    authDomain: "entreapp-7a87c.firebaseapp.com",
+    projectId: "entreapp-7a87c",
+    storageBucket: "entreapp-7a87c.appspot.com",
+    messagingSenderId: "663673157117",
+    appId: "1:663673157117:web:b0f960c9b3e344355a9594",
+    measurementId: "G-RYKXSZ6QPW"
+  }
 };
 
 /*
