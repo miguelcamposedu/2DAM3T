3ce6f59b24d13b9447cd03aeb72dc4f238499191
Miguel Campos <camposmiguel@gmail.com>
2021-04-05 12:14:05 +0200

    Skeleton app

--- skeleton-app/src/app/app-routing.module.ts
+++ skeleton-app/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { AuthComponent } from './layouts/auth/auth.component';
+import { LoginComponent } from './views/auth/login/login.component';
+import { SignupComponent } from './views/auth/signup/signup.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent },
-  { path: "", component: LoginComponent },
-  { path: "**", redirectTo: "", pathMatch: "full" },
+  { 
+    path: "auth", 
+    component: AuthComponent, 
+    children: [
+      { path: "login", component: LoginComponent },
+      { path: "signup", component: SignupComponent },
+      { path: "", redirectTo: "login", pathMatch: "full" }
+    ]
+  },
+  { path: "", redirectTo: "auth", pathMatch: "full" },
+  { path: "**", redirectTo: "auth", pathMatch: "full" },
 ];
 
 @NgModule({
--- skeleton-app/src/app/app.module.ts
+++ skeleton-app/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SignupComponent } from './views/auth/signup/signup.component';
+import { ProfileComponent } from './views/admin/profile/profile.component';
+import { AuthComponent } from './layouts/auth/auth.component';
+import { AdminComponent } from './layouts/admin/admin.component';
+import { LoginComponent } from './views/auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent,
+    AuthComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
